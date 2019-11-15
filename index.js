c5020322a6458d7721ff223c648a0d611474a723
kwon yonggyu <bfsudong@gmail.com>
2019-11-15 15:09:13 +0900

    youdoit cors append

--- index.js
+++ index.js
@@ -21,23 +21,30 @@ passportConfig();
 
 app.use(morgan('dev'));
 
-// var whitelist = ['http://example1.com', 'http://example2.com']
-// var corsOptionsDelegate = function (req, callback) {
-//   var corsOptions;
-//   if (whitelist.indexOf(req.header('Origin')) !== -1) {
-//     corsOptions = { origin: true, credentials: true, } // reflect (enable) the requested origin in the CORS response
-//   }else{
-//     corsOptions = { origin: false } // disable CORS for this request
-//   }
-//   callback(null, corsOptions) // callback expects two parameters: error and options
-// }
+var whitelist = [
+	'http://mygraphr.com',
+	'https://youdoit.space',
+	'http://localhost:3000',
+];
+var corsOptionsDelegate = function(req, callback) {
+	var corsOptions;
+	if (whitelist.indexOf(req.header('Origin')) !== -1) {
+		corsOptions = { origin: true, credentials: true }; // reflect (enable) the requested origin in the CORS response
+	} else {
+		corsOptions = { origin: false }; // disable CORS for this request
+	}
+	callback(null, corsOptions); // callback expects two parameters: error and options
+};
 
 app.use(
-	cors({
+	cors(
+		corsOptionsDelegate,
+		// {
 		// origin: true,
-		origin: 'http://mygraphr.com',
-		credentials: true,
-	}),
+		// origin: 'http://mygraphr.com',
+		// credentials: true,
+		// }
+	),
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
